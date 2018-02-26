cf8c665754dd63e463a46692c7c27ee79fbe1e72
M. Furqan Siddiqui <hello@furqansiddiqui.com>
2018-02-26 11:43:47 +0500

    Authentication

--- src/Authentication.php
+++ src/Authentication.php
@@ -3,7 +3,7 @@
  * This file is a part of "furqansiddiqui/http-client" package.
  * https://github.com/furqansiddiqui/http-client
  *
- * Copyright (c) 2017. Furqan A. Siddiqui <hello@furqansiddiqui.com>
+ * Copyright (c) 2018 Furqan A. Siddiqui <hello@furqansiddiqui.com>
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code or visit following link:
@@ -14,14 +14,17 @@ declare(strict_types=1);
 
 namespace HttpClient;
 
+use HttpClient\Exception\AuthenticationException;
+use HttpClient\Exception\HttpClientException;
+
 /**
  * Class Authentication
  * @package HttpClient
  */
 class Authentication
 {
-    const BASIC =   1;
-    const DIGEST    =   2;
+    public const BASIC = 3001;
+    public const DIGEST = 3002;
 
     /** @var null|int */
     private $type;
@@ -38,19 +41,36 @@ class Authentication
      */
     public function basic(string $username, string $password)
     {
-        $this->type =   self::BASIC;
-        $this->username =   $username;
-        $this->password =   $password;
+        $this->type = self::BASIC;
+        $this->username = $username;
+        $this->password = $password;
+    }
+
+    /**
+     * @param $method
+     * @param $args
+     * @throws AuthenticationException
+     * @throws HttpClientException
+     */
+    public function __call($method, $args)
+    {
+        switch ($method) {
+            case "register":
+                $this->register($args[0] ?? null);
+                return;
+        }
+
+        throw new AuthenticationException(sprintf('Cannot call inaccessible method "%s"', $method));
     }
 
     /**
      * @param $ch
-     * @throws \HttpClientException
+     * @throws AuthenticationException
      */
-    public function register($ch)
+    private function register($ch)
     {
-        if(!is_resource($ch)) {
-            throw new \HttpClientException('First parameter must be a valid cURL resource');
+        if (!is_resource($ch)) {
+            throw new AuthenticationException('Cannot register Authentication opts to a non-resource');
         }
 
         switch ($this->type) {
